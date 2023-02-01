27b190e2d64a64db38c09ab512f5a7dd465a1198
MarcemFlyingmist <114197831+MarcemFlyingmist@users.noreply.github.com>
2023-02-01 16:36:32 -0600

    Add files via upload

---
+++ Js Test.js
@@ -0,0 +1,46 @@
+document.getElementById('purple').onclick = partyPurple
+document.getElementById('blue').onclick = partyBlue
+document.getElementById('green').onclick = partyGreen
+document.querySelector('#pumpkin').addEventListener('click', makeZero)
+document.querySelector('#dominosPizza').addEventListener('click', jumanji)
+document.querySelector('#zebra').addEventListener('click', add9)
+document.querySelector('#cantThinkOfAnything').addEventListener('click', Player_1_Turn)
+
+let total = 0
+let Player_1_Space = 0
+
+function partyPurple(){
+    document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
+    document.querySelector('body').style.color = 'white'
+}
+
+function partyBlue(){
+    document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
+    document.querySelector('body').style.color = 'white'
+}
+
+function partyGreen(){
+    document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
+    document.querySelector('body').style.color = 'white'
+}
+
+function makeZero(){
+    total = 0
+    document.querySelector('#placeToPutResult').innerText = total
+}
+
+function jumanji(){
+    total = total + 3
+    document.querySelector('#placeToPutResult').innerText = total
+}
+
+function add9(){
+    total = total + 9
+    document.querySelector('#placeToPutResult').innerText = total
+}
+
+function Player_1_Turn(){
+    Player_1_Space = Player_1_Space + Math.floor(Math.random() * 6) + 1
+    document.querySelector('#placeToPutResult').innerText = Player_1_Space
+    // cout find"place" in doc make Text with innertext set = total
+}
---
+++ Leon 10.js
@@ -0,0 +1,68 @@
+document.getElementById('purple').onclick = partyPurple
+document.getElementById('blue').onclick = partyBlue
+document.getElementById('green').onclick = partyGreen
+document.querySelector('#pumpkin').addEventListener('click', makeZero)
+document.querySelector('#dominosPizza').addEventListener('click', jumanji)
+document.querySelector('#zebra').addEventListener('click', add9)
+document.querySelector('#cantThinkOfAnything').addEventListener('click', Player_1_Turn)
+document.querySelector('#check').addEventListener('click', check)
+document.querySelector('#hotestChick').addEventListenter('click', hide)
+
+
+let total = 0
+let Player_1_Space = 0
+
+function hide(){
+    //window.alert(BANG)
+    document.querySelector('#Kathy').style.display='none'
+    document.querySelector('#Rachel').style.display='none'
+}
+
+function partyPurple(){
+    document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
+    document.querySelector('body').style.color = 'white'
+}
+
+function partyBlue(){
+    document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
+    document.querySelector('body').style.color = 'white'
+}
+
+function partyGreen(){
+    document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
+    document.querySelector('body').style.color = 'white'
+}
+
+function makeZero(){
+    total = 0
+    document.querySelector('#placeToPutResult').innerText = total
+}
+
+function jumanji(){
+    total = total + 3
+    document.querySelector('#placeToPutResult').innerText = total
+}
+
+function add9(){
+    total = total + 9
+    document.querySelector('#placeToPutResult').innerText = total
+}
+
+function Player_1_Turn(){
+    Player_1_Space = Player_1_Space + Math.floor(Math.random() * 6) + 1
+    document.querySelector('#placeToPutResult').innerText = Player_1_Space
+    // cout find"place" in doc make Text with innertext set = total
+}
+
+function check() {
+    const day = document.querySelector('#day').value.toLowerCase() 
+    //Worked when hardcoded
+    if(day === "tuesday" || day === "thursday"){
+        document.querySelector('#placeToPutDay').innerText = "Class Day"
+    }
+    else if(day === "saturday" || day === "sunday"){
+        document.querySelector('#placeToPutDay').innerText = "Weekend"
+    }
+    else{document.querySelector('#placeToPutDay').innerText = "Boring"}
+
+}
